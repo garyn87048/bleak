f79f8b28488ee86ea3dec130cded01dd73572a0b
Gary N <by_coincidence@yahoo.com>
2024-11-14 22:26:46 -0700

    divide and conquer

--- bleak/backends/bluezdbus/manager.py
+++ bleak/backends/bluezdbus/manager.py
@@ -25,19 +25,26 @@ from typing import (
     cast,
 )
 from weakref import WeakKeyDictionary
+print( f"in \\bleak\\bleak\\backends\\bluezdbus\\manager, imports, marker 1" )
 
 from dbus_fast import BusType, Message, MessageType, Variant, unpack_variants
 from dbus_fast.aio.message_bus import MessageBus
 
+print( f"in \\bleak\\bleak\\backends\\bluezdbus\\manager, imports, marker 2" )
 from ...exc import BleakDBusError, BleakError
 from ..service import BleakGATTServiceCollection
 from . import defs
+print( f"in \\bleak\\bleak\\backends\\bluezdbus\\manager, imports, marker 3" )
+
 from .advertisement_monitor import AdvertisementMonitor, OrPatternLike
 from .characteristic import BleakGATTCharacteristicBlueZDBus
 from .defs import Device1, GattCharacteristic1, GattDescriptor1, GattService1
+print( f"in \\bleak\\bleak\\backends\\bluezdbus\\manager, imports, marker 4" )
 from .descriptor import BleakGATTDescriptorBlueZDBus
 from .service import BleakGATTServiceBlueZDBus
 from .signals import MatchRules, add_match
+print( f"in \\bleak\\bleak\\backends\\bluezdbus\\manager, imports, marker 5" )
+
 from .utils import (
     assert_reply,
     device_path_from_characteristic_path,
